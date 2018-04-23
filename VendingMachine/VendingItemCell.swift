b0dd3804114f8e5e713ade9273743808c61f3efc
Curtis-Stilwell <curtisstilwell1@Curtiss-MacBook-Pro.local>
2018-04-22 21:19:04 -0500

    first commit

---
+++ VendingMachine/VendingItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Curtis Stilwell on 4/14/18.
+//  Copyright © 2018 Treehouse Island, Inc. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconView: UIImageView!
+}
